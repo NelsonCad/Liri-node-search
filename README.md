5bb2bbf1fc8032811c7258c3596e2b1a2c226cc7
NelsonCad <NelsonCad97@gmail.com>
2019-08-09 13:20:12 -0700

    all keys needed are accounted for

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Spotify-node-search
+# Liri-node-search
 learning how to use node.js to search through spotify (heavy metal incoming)
---
+++ keys.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+const fs = require("fs");
+
+console.log("this is loaded");
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+}
+
+exports.omdb = {
+    key: process.env.OMDB_KEY
+}
+
+exports.concerts = {
+    id: codingbootcamp
+}
