9a0e7720a8696a78ef0fb70557bdd283d163e98d
NelsonCad <NelsonCad97@gmail.com>
2019-08-12 18:57:21 -0700

    omdb search is working, concert somewhat is too

--- run.js
+++ run.js
@@ -11,9 +11,9 @@ const concerts = keys.concerts;
 
 
 function songSearch() {
-    let search = process.argv.slice(3).join("%20");
+    let search = process.argv.splice(3).join("%20");
 
-    let queryURL = "https://api.spotify.com/v1/search?q=" + search + "&type=track,album,artist"
+    let queryURL = "https://api.spotify.com/v1/search?q=" + search + "&type=track,album,artist";
 
     axios.get({
         url: queryURL,
@@ -22,13 +22,47 @@ function songSearch() {
             client_secret: spotify.secret
         }
     }).then(function (response) {
-        console.log(JSON.stringify(response))
+        console.log(JSON.stringify(response, null, 2))
+    }).catch(function (err) {
+        console.log(err);
+    });
+};
+
+function concertSearch() {
+    let search = process.argv.splice(3).join("%20");
+
+    let queryURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=" + concerts.id;
+    console.log(queryURL);
+    axios.get(
+        queryURL
+    ).then(function (response) {
+
+        response.forEach(concert) {
+            console.log("\n" + concert.datetime
+                + "\n" + concert.venue.name
+                + "\n" + concert.lineup
+                + "\n" + concert.url);
+        }
+
+    }).catch(function (err) {
+        console.log(err);
+    });
+};
+
+function movieSearch() {
+    let search = process.argv.splice(3).join("%20");
+
+    let queryURL = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=" + omdb.key;
+
+    axios.get(queryURL).then(function (response) {
+        console.log("\nthe movie was released in " + response.data.Released);
+        console.log("has a rating of " + response.data.imdbRating);
+        console.log("the plot is: ");
+        console.log(response.data.Plot);
+    }).catch(function (err) {
+        console.log(err);
     });
 }
-
-
-
-
 
 switch (process.argv[2]) {
     case "spotify":
@@ -36,4 +70,14 @@ switch (process.argv[2]) {
     case "search-Song":
         songSearch();
         break;
-}
+    case "concerts":
+    case "concert-this":
+    case "bandsintown":
+        concertSearch();
+        break;
+    case "omdb":
+    case "movie":
+    case "search-movie":
+        movieSearch();
+        break;
+};
